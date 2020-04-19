00b01bd63484beededa6bcc45965700926990abb
irfannurahman08 <irfannurahman082gmail.com>
2020-04-19 18:25:38 +0700

    done

---
+++ App_Code/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public double GAJI(int gapok, int tunjangan, int potongan, double Pph)
+    {
+        double PPH = Pph / 100;
+        double pajak = PPH * gapok;
+        double total = ((gapok + tunjangan) - potongan) - pajak;
+        return total;
+    }
+}
